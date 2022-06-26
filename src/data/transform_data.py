3b0ff4b47826e9b7ccf9234b244db47978c0268c
Nicolas <nicolas.mosquerar@hotmail.com>
2022-06-26 15:03:41 -0500

    se cambia la estructura de transform.py por completo

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -7,29 +7,37 @@
 
 """
 import pandas as pd
-import glob
+#import glob
 import os
 import shutil
 
 def transform_data():   
-    destino= r'C:\Users\Nicolás\github-classroom\productos-de-datos\proyecto-nicolasmosquerar\data_lake\raw'
-    os.chdir('data_lake/landing/')
-    ruta=r'C:\Users\Nicolás\github-classroom\productos-de-datos\proyecto-nicolasmosquerar\data_lake\landing'
-    for excel in os.listdir(ruta):
-        if excel.endswith('.xlsx'):
-            out = excel.split('.')[0]+'.csv'
-            df=pd.read_excel(excel)
-            df.to_csv(out,index=False)
-        
-    for excel in os.listdir(ruta):
-        if excel.endswith('.xls'):
-            out = excel.split('.')[0]+'.csv'
-            df=pd.read_excel(excel)
-            df.to_csv(out,index=False)
-        
-    for csv in os.listdir(ruta):
-        if csv.endswith('.csv'):
-            shutil.move(f"{csv}", destino)
+  for file in range(1995,2022):
+      
+      if file in range(1995,2000):
+          read_file = pd.read_excel(
+              'data_lake/landing/{}.xlsx'.format(file),header= 3)
+          read_file.to_csv(
+                  'data_lake/raw/{}.csv'.format(file),index=None )
+      elif file in range(2000,2018):
+          if file in [2016,2017]:
+              read_file=pd.read_excel(
+                  'data_lake/landing/{}.xls'.format(file),header= 2)
+              read_file.to_csv('data_lake/raw/{}.csv'.format(file),index=None)
+          else:
+              read_file = pd.read_excel(
+              'data_lake/landing/{}.xlsx'.format(file),header= 3)
+          read_file.to_csv(
+                  'data_lake/raw/{}.csv'.format(file),index=None )
+          
+      else:
+                read_file = pd.read_excel(
+              'data_lake/landing/{}.xlsx'.format(file),header= 0)
+                read_file.to_csv(
+              'data_lake/raw/{}.csv'.format(file),index=None ) 
+              
+           
+  
 
 #'data_lake/raw/{}.csv'.format(excel)
 if __name__ == "__main__":
