6e30a2229723198eb0d4b932eb0c2538dcfea6b7
Nicolas <nicolas.mosquerar@hotmail.com>
2022-06-26 16:12:36 -0500

    se desarrollan los puntos hasta antes del pipeline

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -1,5 +1,4 @@
-def compute_daily_prices():
-    """Compute los precios promedios diarios.
+"""Compute los precios promedios diarios.
 
     Usando el archivo data_lake/cleansed/precios-horarios.csv, compute el prcio
     promedio diario (sobre las 24 horas del dia) para cada uno de los dias. Las
@@ -11,11 +10,18 @@ def compute_daily_prices():
 
 
 
-    """
-    raise NotImplementedError("Implementar esta función")
+"""
+
+import pandas as pd
+import numpy as np
+def compute_daily_prices():
+    df=pd.read_csv('data_lake/cleansed/precios-horarios.csv',index_col=None, parse_dates=['fecha'])
+    media=df.groupby(['fecha'])['precio'].mean()
+    #print(media)
+    media.to_csv('data_lake/business/precios-diarios.csv')
 
 
 if __name__ == "__main__":
     import doctest
-
+    compute_daily_prices()
     doctest.testmod()
--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -1,5 +1,4 @@
-def compute_monthly_prices():
-    """Compute los precios promedios mensuales.
+"""Compute los precios promedios mensuales.
 
     Usando el archivo data_lake/cleansed/precios-horarios.csv, compute el prcio
     promedio mensual. Las
@@ -12,10 +11,16 @@ def compute_monthly_prices():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+import pandas as pd
+def compute_monthly_prices():
+   
+    df=pd.read_csv('data_lake/cleansed/precios-horarios.csv',index_col=None, parse_dates=['fecha'])
+    media=df.groupby(pd.Grouper(freq='1M', key='fecha'))['precio'].mean().dropna()
+    print(media)
+    #media.to_csv('data_lake/business/precios-diarios.csv')
 
 
 if __name__ == "__main__":
     import doctest
-
+    compute_monthly_prices()
     doctest.testmod()
