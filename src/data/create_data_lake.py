e6fdbf20a6de4dd022878a7577281336a0123e0d
Nicolas <nicolas.mosquerar@hotmail.com>
2022-06-29 22:08:21 -0500

    cambios en autograding

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -26,34 +26,17 @@ import os
 
 def create_data_lake():
     
-    directorio = "./data_lake"
-    directorio_landing = "./data_lake/landing"
-    directorio_raw = "./data_lake/raw"
-    directorio_cleansed = "./data_lake/cleansed"
-    directorio_business = "./data_lake/business"
-    directorio_business_features = "./data_lake/business/features"
-    directorio_business_forecast = "./data_lake/business/forecasts" 
-    directorio_business_reports= "./data_lake/business/reports/figures"
-     
-    if os.path.exists("./data_lake")==False:   
-        os.mkdir(directorio)
-        os.mkdir(directorio_landing)
-        os.mkdir(directorio_raw)
-        os.mkdir(directorio_cleansed)
-        os.mkdir(directorio_business)
-        os.mkdir(directorio_business_features)
-        os.mkdir(directorio_business_forecast)
-        os.makedirs(directorio_business_reports)
-    else:
-        os.removedirs(directorio_business_reports)
-        os.rmdir(directorio_business_features)
-        os.rmdir(directorio_business_forecast)
-        os.rmdir(directorio_landing)
-        os.rmdir(directorio_raw)
-        os.rmdir(directorio_cleansed)
-        os.rmdir(directorio_business)
-        os.rmdir(directorio)
     
+    os.mkdir("./data_lake")
+    carpetas = ["landing", "raw", "cleansed", "business"]
+    subcarp_bus = ["reports", "features", "forecasts"]
+    subcarp_bus_reportes = ["figures"]
+    raiz = [os.mkdir(os.path.join("./data_lake/", each_dir))
+     for each_dir in carpetas]
+    business = [os.mkdir(os.path.join("./data_lake/business/", each_dir))
+     for each_dir in subcarp_bus]
+    reportes = [os.mkdir(os.path.join("./data_lake/business/reports/", each_dir))
+     for each_dir in subcarp_bus_reportes]
 
 if __name__ == "__main__":
     import doctest
