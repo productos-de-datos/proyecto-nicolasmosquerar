4b8d2108cc53de54c30703bdff736e0c0bc02d9c
Nicolas <nicolas.mosquerar@hotmail.com>
2022-06-29 22:31:45 -0500

    a

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -23,7 +23,7 @@
 
 
 import os
-import shutil
+
 def create_data_lake():
     
     directorio = "./data_lake"
@@ -45,7 +45,13 @@ def create_data_lake():
         os.mkdir(directorio_business_forecast)
         os.makedirs(directorio_business_reports)
     else:
-        shutil.rmtree("./data_lake",ignore_errors=True)
+        os.removedirs(directorio_business_reports)
+        os.rmdir(directorio_business_features)
+        os.rmdir(directorio_business_forecast)
+        os.rmdir(directorio_landing)
+        os.rmdir(directorio_raw)
+        os.rmdir(directorio_cleansed)
+        os.rmdir(directorio_business)
         os.rmdir(directorio)
     
 
